c374913f105400663026ed5256e3c0bb1ee4a71d
silvermannn <227780093+silvermannn@users.noreply.github.com>
2026-03-16 20:47:41 +0300

    Added zip full for ordered lists

--- app/Data/Common.hs
+++ app/Data/Common.hs
@@ -7,3 +7,12 @@ deleteN n xs = as ++ drop 1 bs
 
 replaceN :: Int -> a -> [a] -> [a]
 replaceN n x xs = x : deleteN n xs
+
+zipFull :: Ord a => (a -> b) -> (a -> a -> b) -> [a] -> [a] -> [b]
+zipFull f _ xs [] = map f xs
+zipFull f _ [] ys = map f ys
+zipFull f g (x:xs) (y:ys) =
+  case compare x y of
+    LT -> f x : zipFull f g xs (y : ys)
+    EQ -> g x y : zipFull f g xs ys
+    GT -> f y : zipFull f g (x : xs) ys
